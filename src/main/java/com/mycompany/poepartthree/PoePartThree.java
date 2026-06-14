ba43bd7aafffc3b0df352ae382f8531bbdb98994
Khanya-Maqaqa <>
2026-06-14 12:10:52 +0200

    first commit

---
+++ src/main/java/com/mycompany/poepartthree/PoePartThree.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poepartthree;
+
+/**
+ *
+ * @author kvmaq
+ */
+public class PoePartThree {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
